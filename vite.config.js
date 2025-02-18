f4fa9416a19f4d91a7331cc9b922f1a45d78df2c
fabio7621 <d6200121a@gmail.com>
2025-02-18 16:58:39 +0800

    little fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/ReactWeek-homework/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/react-work5/" : "/",
   plugins: [react()],
 });
